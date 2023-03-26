e234acc0433d3388f205185eb556c8bd308d1ba3
Joshua <jzhu709@aucklanduni.ac.nz>
2023-03-27 00:15:36 +1300

    Updated user dtos and models

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -7,6 +7,6 @@ namespace OpulentOysters.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
-        public string UserName { get; set; } = null!;
+        public string Username { get; set; } = null!;
     }
 }
