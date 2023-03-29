8169dcadfe116b2582b2ebf8a6b053d989968623
mlin538 <79815298+mlin538@users.noreply.github.com>
2023-03-29 21:03:34 +1300

    add Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { Dashboard } from "./pages/Dashboard/Dashboard";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import { LandingPage } from "./pages/LandingPage/LandingPage";
+import { Login } from "./pages/Login/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  }, 
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
