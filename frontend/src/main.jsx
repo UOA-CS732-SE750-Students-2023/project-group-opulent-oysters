7922ddc765c52574b49806e06edfd9e5ad7365ab
zwu238 <zwu238@aucklanduni.ac.nz>
2023-03-29 21:10:16 +1300

    removed app.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
 import "./index.css";
 import { Dashboard } from "./pages/Dashboard/Dashboard";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
@@ -17,11 +16,11 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
-  }, 
+  },
   {
     path: "/dashboard",
     element: <Dashboard />,
-  }
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
