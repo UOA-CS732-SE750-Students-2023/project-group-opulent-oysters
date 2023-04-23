5ba9b0bd70f88b6b9189ec5f96e46239a9f48f57
gavinmackintosh <79814703+gavinmackintosh@users.noreply.github.com>
2023-04-23 19:30:06 +1200

    Added spotify code getting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const authUrl = "https://accounts.spotify.com/authorize";
+const clientId = "cddea26bbe4a468bae595c6581073ec2";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  if (window.location.search.length > 0) { //If query params in URL TODO: this also needs a check if there is a user currently signed in
+      handleRedirect();
+  }
 
   return (
     <div className="App">
@@ -21,6 +28,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={authUser}>
+            Login to spotify
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -30,6 +40,32 @@ function App() {
       </p>
     </div>
   )
+}
+
+function handleRedirect() {
+    let code = getCodeFromUri();
+    // TODO: send code to BE
+    console.log(code);
+}
+
+function getCodeFromUri() {
+    let code = null;
+    const queryString = window.location.search;
+    if (queryString.length > 0) {
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get("code");
+    }
+    return code;
+}
+
+function authUser() {
+    let url = authUrl;
+    url += "?client_id=" + clientId;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI("http://localhost:5173");
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    window.location.href = url;
 }
 
 export default App
