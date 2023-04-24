07db385203a6e96451cf8e19eaa2f0d5f3d6f540
Joshua <jzhu709@aucklanduni.ac.nz>
2023-04-24 23:56:26 +1200

    Fixed bug to get spotify token

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -62,7 +62,7 @@ function authUser() {
     let url = authUrl;
     url += "?client_id=" + clientId;
     url += "&response_type=code";
-    url += "&redirect_uri=" + encodeURI("http://localhost:5173");
+    url += "&redirect_uri=" + encodeURI("http://localhost:5173/");
     url += "&show_dialog=true";
     url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
     window.location.href = url;
