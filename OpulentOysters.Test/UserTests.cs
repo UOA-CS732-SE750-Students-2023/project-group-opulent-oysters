228d02715cf8b46f59fd37d723bf4997afd15f38
gavinmackintosh <79814703+gavinmackintosh@users.noreply.github.com>
2023-05-12 23:06:33 +1200

    added token refreshing (#63)
    * added token refreshing
    
    * Fixed tests on update
    
    * Added recursive return
    
    * Fixed tests
    
    ---------
    
    Co-authored-by: Joshua <jzhu709@aucklanduni.ac.nz>

--- OpulentOysters.Test/UserTests.cs
+++ OpulentOysters.Test/UserTests.cs
@@ -26,8 +26,9 @@ namespace OpulentOysters.Test
             // Arrange
             var mockMongoDb = new Mock<MongoDbService>();
             mockMongoDb.Setup(x => x.CreateUser(It.IsAny<User>()));
+            var mockSpotifySettings = new Mock<IOptions<SpotifySettings>>();
 
-            var controller = new UserController(mockMongoDb.Object);
+            var controller = new UserController(mockMongoDb.Object, mockSpotifySettings.Object);
 
             // Act
             var result = await controller.CreateUser(dummyUserDto);
@@ -53,8 +54,9 @@ namespace OpulentOysters.Test
             var mockMongoDb = new Mock<MongoDbService>();
             mockMongoDb.Setup(x => x.GetTokenFromRoomId("696969"))
                 .Returns(GetDummyToken);
+            var mockSpotifySettings = new Mock<IOptions<SpotifySettings>>();
 
-            var controller = new UserController(mockMongoDb.Object);
+            var controller = new UserController(mockMongoDb.Object, mockSpotifySettings.Object);
 
             // Act
             var result = await controller.SearchSong("Hello", "696969");
@@ -94,8 +96,9 @@ namespace OpulentOysters.Test
                 .ReturnsAsync(GetDummyOrderNumber);
             mockMongoDb.Setup(x => x.GetQueue("696969"))
                 .ReturnsAsync(GetTestSongs);
+            var mockSpotifySettings = new Mock<IOptions<SpotifySettings>>();
 
-            var controller = new UserController(mockMongoDb.Object);
+            var controller = new UserController(mockMongoDb.Object, mockSpotifySettings.Object);
 
             // Act
             var result = await controller.AddSong("3r8RuvgbX9s7ammBn07D3W", "696969", "dummyUser");
@@ -131,8 +134,9 @@ namespace OpulentOysters.Test
             var mockMongoDb = new Mock<MongoDbService>();
             mockMongoDb.Setup(x => x.UpvoteSong("696969", "dummyTrackId", "dummyUser123"))
                 .ReturnsAsync(GetSuccessfulSongVoteResponse);
+            var mockSpotifySettings = new Mock<IOptions<SpotifySettings>>();
 
-            var controller = new UserController(mockMongoDb.Object);
+            var controller = new UserController(mockMongoDb.Object, mockSpotifySettings.Object);
 
             // Act
             var result = await controller.UpvoteSong("dummyTrackId", "696969", "dummyUser123");
@@ -159,8 +163,9 @@ namespace OpulentOysters.Test
             var mockMongoDb = new Mock<MongoDbService>();
             mockMongoDb.Setup(x => x.UpvoteSong("696969", "notFoundId", "dummyUser123"))
                 .ReturnsAsync(GetNotFoundSongVoteResponse);
+            var mockSpotifySettings = new Mock<IOptions<SpotifySettings>>();
 
-            var controller = new UserController(mockMongoDb.Object);
+            var controller = new UserController(mockMongoDb.Object, mockSpotifySettings.Object);
 
             // Act
             var result = await controller.UpvoteSong("notFoundId", "696969", "dummyUser123");
@@ -188,8 +193,9 @@ namespace OpulentOysters.Test
             var mockMongoDb = new Mock<MongoDbService>();
             mockMongoDb.Setup(x => x.UpvoteSong("696969", "alreadyLikedId", "dummyUser123"))
                 .ReturnsAsync(GetAlreadyLikedSongVoteResponse);
+            var mockSpotifySettings = new Mock<IOptions<SpotifySettings>>();
 
-            var controller = new UserController(mockMongoDb.Object);
+            var controller = new UserController(mockMongoDb.Object, mockSpotifySettings.Object);
 
             // Act
             var result = await controller.UpvoteSong("alreadyLikedId", "696969", "dummyUser123");
@@ -217,8 +223,9 @@ namespace OpulentOysters.Test
             var mockMongoDb = new Mock<MongoDbService>();
             mockMongoDb.Setup(x => x.DownvoteSong("696969", "dummyTrackId", "dummyUser123"))
                 .ReturnsAsync(GetSuccessfulSongVoteResponse);
+            var mockSpotifySettings = new Mock<IOptions<SpotifySettings>>();
 
-            var controller = new UserController(mockMongoDb.Object);
+            var controller = new UserController(mockMongoDb.Object, mockSpotifySettings.Object);
 
             // Act
             var result = await controller.DownvoteSong("dummyTrackId", "696969", "dummyUser123");
@@ -240,8 +247,9 @@ namespace OpulentOysters.Test
             var mockMongoDb = new Mock<MongoDbService>();
             mockMongoDb.Setup(x => x.DownvoteSong("696969", "notFoundId", "dummyUser123"))
                 .ReturnsAsync(GetNotFoundSongVoteResponse);
+            var mockSpotifySettings = new Mock<IOptions<SpotifySettings>>();
 
-            var controller = new UserController(mockMongoDb.Object);
+            var controller = new UserController(mockMongoDb.Object, mockSpotifySettings.Object);
 
             // Act
             var result = await controller.DownvoteSong("notFoundId", "696969", "dummyUser123");
@@ -264,8 +272,9 @@ namespace OpulentOysters.Test
             var mockMongoDb = new Mock<MongoDbService>();
             mockMongoDb.Setup(x => x.DownvoteSong("696969", "alreadyLikedId", "dummyUser123"))
                 .ReturnsAsync(GetAlreadyDislikedSongVoteResponse);
+            var mockSpotifySettings = new Mock<IOptions<SpotifySettings>>();
 
-            var controller = new UserController(mockMongoDb.Object);
+            var controller = new UserController(mockMongoDb.Object, mockSpotifySettings.Object);
 
             // Act
             var result = await controller.DownvoteSong("alreadyLikedId", "696969", "dummyUser123");
--- frontend/src/components/__tests__/LyricLine.test.jsx
+++ frontend/src/components/__tests__/LyricLine.test.jsx
@@ -1,17 +1,7 @@
 import "@testing-library/jest-dom";
-import { describe, expect, it } from "vitest";
+import { it } from "vitest";
 import { render } from "@testing-library/react";
 import { LyricLine } from "../LyricLine";
-
-it("renders the component correctly when the line is supplied", () => {
-    const dummyLine = {
-        words: "Hello, its me",
-    };
-    const { getByText } = render(
-        <LyricLine line={dummyLine} />
-    );
-    expect(getByText("Hello, its me")).toBeInTheDocument();
-});
 
 it("fails to render when no content is supplied", () => {
     try {
